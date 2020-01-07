c549b32b999b7a581572cc2c2637fa283d09cd2f
Paramastri Ardiningrum <paramastriii@gmail.com>
2020-01-07 14:10:30 +0700

    first commit + view

---
+++ index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Status Dokumen OBL</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="icon" href="favicon.png" type="png" sizes="16x16">
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="style5.css">
+
+
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    <script>
+        $(function() {
+
+            $(".dropdown-menu").on('click', 'a', function() {
+                $(".btn:first-child").text($(this).text());
+                $(".btn:first-child").val($(this).text());
+            });
+
+        });
+
+        $(document).ready(function() {
+            $('#sidebarCollapse').on('click', function() {
+                $('#sidebar').toggleClass('active');
+                $(this).toggleClass('active');
+            });
+        });
+    </script>
+
+</head>
+
+
+
+<body>
+
+
+    <div class="wrapper">
+        <!-- Sidebar Holder -->
+        <nav id="sidebar">
+            <img style="height: 100px; margin-top: 30px;" src="logo.png" class="rounded mx-auto d-block">
+            <div class="sidebar-header">
+            <h6 style="text-align: center; color: black; background-color: white; border-radius: 30px; width: 90%;">Website Status OBL</h6>
+            </div>
+
+
+            <ul style="margin-left: 10px; margin-top: 30px;" class="list-unstyled">
+
+                <li>
+                    <a href="#">Form</a>
+                </li>
+                <li>
+                    <a href="#">Data</a>
+                </li>
+            </ul>
+
+
+
+
+        </nav>
+
+
+
+        <div id="content">
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="">
+
+                    <button style="margin-right: 30px;" type="button" id="sidebarCollapse" class="navbar-btn">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
+                    <h2 style="font-family:'GothamRounded-Medium'; float: right;">Form Status Dokumen OBL</h2>
+                    <!--  <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-align-justify"></i>
+                    </button> -->
+
+
+
+                </div>
+            </nav>
+
+            <div>
+            <form action="{{ url("storelogin") }}" method = "post" style="margin-left: 90px; margin-top: 50px; width: 30%; font-family:'GothamRounded-Medium';">
+
+                <div class="form-group">
+                    <label>Nama CC</label>
+                    <input type="text" class="form-control" placeholder="Masukkan Nama CC" name="" required>
+                </div>
+
+                <div class="form-group">
+                    <label>Nama Mitra</label>
+                    <input type="text" class="form-control" placeholder="Masukkan Nama Mitra" name="" required>
+                </div>
+
+                <div class="form-group">
+                    <label>Nama Pekerjaan</label>
+                    <input type="text" class="form-control" placeholder="Masukkan Nama Pekerjaan" name="" required>
+                </div>
+
+                <div class="form-group">
+                    <label>Produk</label>
+                    <input type="text" class="form-control" placeholder="Masukkan Produk" name="" required>
+                </div>
+
+                <div class="form-group">
+                    <label>PIC Mitra</label>
+                    <input type="text" class="form-control" placeholder="Masukkan PIC Mitra" name="" required>
+                </div>
+
+                <div>
+                    <label style="margin-top: 0px;">P6</label>
+                </div>
+
+                <input style="font-size: 10pt;" type="file" name="" required="">
+
+                <select name="" class="form-control form-control-sm" style="width: 100%; font-size: 15pt; margin-top: 20px;" required>
+                  <option value="0"></option>
+                  <option value="1">OK</option>
+                  <option value="2">Belum OK</option>
+                </select> 
+
+                <!-- Keterangan If Belum OK  -->
+
+                <div class="form-group">
+                    <label style="margin-top: 20px;" for="exampleFormControlTextarea1">Keterangan</label>
+                    <textarea class="form-control" placeholder="Masukkan Keterangan..." id="exampleFormControlTextarea1" rows="3" required></textarea>
+                </div>
+
+
+                <div>
+                    <label style="margin-top: 15px;">P8</label>
+                </div>
+
+                <input style="font-size: 10pt;" type="file" name="" required="">
+
+                <select name="" class="form-control form-control-sm" style="width: 100%; font-size: 15pt; margin-top: 20px;" required>
+                  <option value="0"></option>
+                  <option value="1">OK</option>
+                  <option value="2">Belum OK</option>
+                </select> 
+
+                <!-- Keterangan If Belum OK  -->
+
+                <div class="form-group">
+                    <label style="margin-top: 20px;" for="exampleFormControlTextarea1">Keterangan</label>
+                    <textarea class="form-control" placeholder="Masukkan Keterangan..." id="exampleFormControlTextarea1" rows="3" required></textarea>
+                </div>
+
+                <div>
+                    <label style="margin-top: 15px;">KL</label>
+                </div>
+
+                <input style="font-size: 10pt;" type="file" name="" required="">
+
+                <select name="" class="form-control form-control-sm" style="width: 100%; font-size: 15pt; margin-top: 20px;" required>
+                  <option value="0"></option>
+                  <option value="1">OK</option>
+                  <option value="2">Belum OK</option>
+                </select> 
+
+                <!-- Keterangan If Belum OK  -->
+
+                <div class="form-group">
+                    <label style="margin-top: 20px;" for="exampleFormControlTextarea1">Keterangan</label>
+                    <textarea class="form-control" placeholder="Masukkan Keterangan..." id="exampleFormControlTextarea1" rows="3" required></textarea>
+                </div>
+
+                <div>
+                    <label style="margin-top: 15px;">BAST Mitra</label>
+                </div>
+
+                <input style="font-size: 10pt;" type="file" name="" required="">
+
+                <select name="" class="form-control form-control-sm" style="width: 100%; font-size: 15pt; margin-top: 20px;" required>
+                  <option value="0"></option>
+                  <option value="1">OK</option>
+                  <option value="2">Belum OK</option>
+                </select> 
+
+                <!-- Keterangan If Belum OK  -->
+
+                <div class="form-group">
+                    <label style="margin-top: 20px;" for="exampleFormControlTextarea1">Keterangan</label>
+                    <textarea class="form-control" placeholder="Masukkan Keterangan..." id="exampleFormControlTextarea1" rows="3" required></textarea>
+                </div>
+
+                <button value = "" style="margin-top: 40px; margin-bottom: 30px;" type="submit" class="btn btn-primary">Selesai</button>
+            </form>
+            </div>
+
+
+            
+            
+            </form>
+
+
+</body>
+
+</html>
