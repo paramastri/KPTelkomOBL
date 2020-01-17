4d104b389923123c1b91ca1cd7e7311e6407d635
Lutfiy <fiyanti33@gmail.com>
2020-01-17 09:40:30 +0700

    model

---
+++ app/models/file.php
@@ -0,0 +1,27 @@
+<?php
+use Phalcon\Mvc\Model;
+class file extends Model
+{
+    public $id;
+    public $file_p0;
+    public $status_p0;
+    public $keterangan_p0;
+    public $file_p1;
+    public $status_p1;
+    public $keterangan_p1;
+    public $file_p2;
+    public $status_p2;
+    public $keterangan_p2;
+    public $file_p6;
+    public $status_p6;
+    public $keterangan_p6;
+    public $file_p8;
+    public $status_p8;
+    public $keterangan_p8;
+    public $file_kl;
+    public $status_kl;
+    public $keterangan_kl;
+    public $file_bast;
+    public $status_bast;
+    public $keterangan_bast;
+}
---
+++ app/models/obl.php
@@ -0,0 +1,11 @@
+<?php
+use Phalcon\Mvc\Model;
+class obl extends Model
+{
+    public $id;
+    public $id_file;
+    public $nama_cc;
+    public $nama_mitra;
+    public $nama_pekerjaan;
+    public $pic_mitra
+}
